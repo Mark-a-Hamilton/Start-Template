bafddeda852031125acb10171c9faffbb6549ecc
Mark-a-Hamilton <154675184+Mark-a-Hamilton@users.noreply.github.com>
2024-02-27 12:25:17 +0000

    27/02/2024 - remove API

---
+++ API/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+// Routing
+
+// "/Log"
+app.MapGet("/log", () =>
+{
+    return "Get All the log entries";
+})
+app.Run();
